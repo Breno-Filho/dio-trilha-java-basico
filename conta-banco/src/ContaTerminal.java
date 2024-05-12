3b66d107bf1d70119f4924eebfedd9e087af9883
Breno Filho <brenoo.filho@unifacol.edu.br>
2024-05-12 20:06:05 -0300

    Meu projeto de conta de banco - Bootcamp Java Santander 2024

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -23,6 +23,6 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
 
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
